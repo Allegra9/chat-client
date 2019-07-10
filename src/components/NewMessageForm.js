978fddf6c623e21bfbd8d46df39a0635d2f2ec64
Allegra <aukseallegra@gmail.com>
2019-07-10 16:18:06 +0100

    add emoji update

--- src/components/NewMessageForm.js
+++ src/components/NewMessageForm.js
@@ -1,79 +1,79 @@
-import React from 'react'
-import { postMessage } from '../adapter/api'
-import validate from './validations'
+import React from "react";
+import { postMessage } from "../adapter/api";
+import validate from "./validations";
 
-import 'emoji-mart/css/emoji-mart.css'
-import { Picker } from 'emoji-mart'
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
 
 class NewMessageForm extends React.Component {
-
-  state={
-    text: '',
+  state = {
+    text: "",
     conversation_id: this.props.conversation_id,
     user_id: this.props.user_id,
     showEmojis: false,
     errors: []
-  }
+  };
 
-  showEmojis = (e) => {
-    this.setState({
-      showEmojis: true
-    }, () => document.addEventListener('click', this.closeMenu))
-  }
+  showEmojis = e => {
+    this.setState(
+      {
+        showEmojis: true
+      },
+      () => document.addEventListener("click", this.closeMenu)
+    );
+  };
 
-  closeMenu = (e) => {
-    console.log(this.emojiPicker)
+  closeMenu = e => {
+    console.log(this.emojiPicker);
     if (this.emojiPicker !== null && !this.emojiPicker.contains(e.target)) {
-      this.setState({
-        showEmojis: false
-      }, () => document.removeEventListener('click', this.closeMenu))
+      this.setState(
+        {
+          showEmojis: false
+        },
+        () => document.removeEventListener("click", this.closeMenu)
+      );
     }
-  }
+  };
 
-  componentWillReceiveProps = (nextProps) => {
+  componentWillReceiveProps = nextProps => {
     this.setState({
       conversation_id: nextProps.conversation_id,
       user_id: nextProps.user_id
-    })
-  }
+    });
+  };
 
-  handleChange = (e) => {
-    let validation = validate(e.target.value)
+  handleChange = e => {
+    let validation = validate(e.target.value);
     //console.log(validation)
     this.setState({
       text: e.target.value,
       errors: validation
-    })
-  }
+    });
+  };
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    let validation = validate(this.state.text)
+  handleSubmit = e => {
+    e.preventDefault();
+    let validation = validate(this.state.text);
     //console.log(validation)
-    if (validation.length === 0){
-      postMessage(this.state)
-      this.setState({ text: '' })
+    if (validation.length === 0) {
+      postMessage(this.state);
+      this.setState({ text: "" });
     }
     this.setState({
       errors: validation
-    })
-  }
+    });
+  };
 
-  addEmoji = (e) => {
-    console.log(e.unified)
-    let sym = e.unified.split('-')
-    let codesArray = []
-    sym.forEach(el => codesArray.push('0x' + el))
-      //console.log(codesArray)  // ["0x1f3f3", "0xfe0f"]
-    let emojiPic = String.fromCodePoint(...codesArray) //("0x1f3f3", "0xfe0f")
+  addEmoji = e => {
+    // console.log(e.native);
+    let emoji = e.native;
     this.setState({
-      text: this.state.text + emojiPic
-    })
-  }
+      text: this.state.text + emoji
+    });
+  };
 
   render() {
-
-    let errors = this.state.errors.map(err => <p>{err}</p> )
+    let errors = this.state.errors.map(err => <p>{err}</p>);
 
     return (
       <div style={styles.container} className="newMessageForm">
@@ -83,66 +83,69 @@ class NewMessageForm extends React.Component {
             type="text"
             value={this.state.text}
             onChange={this.handleChange}
-            placeholder="Type a message here then hit ENTER"
+            placeholder="Type a message and hit ENTER"
           />
         </form>
-        <div style={{"color": "red"}}>{errors}</div>
-        {
-          this.state.showEmojis ?
-            <span style={styles.emojiPicker} ref={el => (this.emojiPicker = el)}>
-              <Picker onSelect={this.addEmoji} />
-            </span>
-          :
-            <p style={styles.getEmojiButton} onClick={this.showEmojis} >
-              {String.fromCodePoint(0x1f60a)}
-            </p>
-        }
+        <div style={{ color: "red" }}>{errors}</div>
+        {this.state.showEmojis ? (
+          <span style={styles.emojiPicker} ref={el => (this.emojiPicker = el)}>
+            <Picker
+              onSelect={this.addEmoji}
+              emojiTooltip={true}
+              title="weChat"
+            />
+          </span>
+        ) : (
+          <p style={styles.getEmojiButton} onClick={this.showEmojis}>
+            {String.fromCodePoint(0x1f60a)}
+          </p>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default NewMessageForm
+export default NewMessageForm;
 
 const styles = {
   container: {
     padding: 20,
-    borderTop: '1px #4C758F solid',
-    marginBottom: 20,
+    borderTop: "1px #4C758F solid",
+    marginBottom: 20
   },
   form: {
-    display: 'flex',
+    display: "flex"
   },
   input: {
-    color: 'inherit',
-    background: 'none',
-    outline: 'none',
-    border: 'none',
+    color: "inherit",
+    background: "none",
+    outline: "none",
+    border: "none",
     flex: 1,
-    fontSize: 16,
+    fontSize: 16
   },
   getEmojiButton: {
-    cssFloat: 'right',
-    border: 'none',
+    cssFloat: "right",
+    border: "none",
     margin: 0,
-    cursor: 'pointer',
+    cursor: "pointer"
   },
   emojiPicker: {
-   position: 'absolute',
-   bottom: 10,
-   right: 0,
-   cssFloat: 'right',
-   marginLeft: '200px',
- }
-}
+    position: "absolute",
+    bottom: 10,
+    right: 0,
+    cssFloat: "right",
+    marginLeft: "200px"
+  }
+};
 
 const customEmojis = [
   {
-    name: 'Octocat',
-    short_names: ['octocat'],
-    text: '',
+    name: "Octocat",
+    short_names: ["octocat"],
+    text: "",
     emoticons: [],
-    keywords: ['github'],
-    imageUrl: 'https://assets-cdn.github.com/images/icons/emoji/octocat.png?v7',
-  },
-]
+    keywords: ["github"],
+    imageUrl: "https://assets-cdn.github.com/images/icons/emoji/octocat.png?v7"
+  }
+];
